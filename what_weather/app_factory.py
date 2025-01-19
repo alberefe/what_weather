2d21cc221d55103123d7c7e2df115e996af48839
alberefe <alberefe@tutanota.com>
2025-01-19 20:24:58 +0100

    Config changes.

--- what_weather/app_factory.py
+++ what_weather/app_factory.py
@@ -1,53 +1,39 @@
-from flask import Flask, redirect, url_for
+import json
 import os
-from flask.cli import load_dotenv
+from urllib import request
+
+import requests
+
+from what_weather import config
+from what_weather.config import config
+from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 # Create the object to deal with the database
 db = SQLAlchemy()
 
 
-def create_app(test_config=None):
-    load_dotenv()
-
+def create_app(config_name=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        # the key should be changed in deployment
-        SECRET_KEY=os.getenv("SECRET_KEY"),
-        WEATHERSTACK_API_KEY=os.getenv("WEATHERSTACK_API_KEY"),
-        REDIS_URL=os.getenv("REDIS_URL"),
-        SQLALCHEMY_DATABASE_URI="postgresql+psycopg://{user}:{password}@{host}:{port}/{dbname}".format(
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASSWORD"),
-            host=os.getenv("DB_HOST"),
-            port=os.getenv("DB_PORT"),
-            dbname=os.getenv("DB_NAME"),
-        ),
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        DEBUG = True,
-    )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        # it forces it to get it from config.py
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+    if config_name is None:
+        config_name = "development_config"
 
+    app.config.from_object(config[config_name])
+
+    # Connect the database to the flask app
     db.init_app(app)
 
-    import what_weather.database_models
+    from what_weather import database_models
 
-    # If the tables don't exist, create them
+    # Create tables of the database
     with app.app_context():
         db.create_all()
 
     from what_weather import redis_cache
 
     redis_cache.init_app(app)
-
 
     @app.route("/")
     def index():
@@ -60,5 +46,47 @@ def create_app(test_config=None):
     from what_weather import weather
 
     app.register_blueprint(weather.bp)
+
+
+    # TODO: find a nice place to leave this
+    @app.route("/webhook", methods=["POST"])
+    def bot_webhook():
+        if request.method == "POST":
+            req = request.get_json()
+
+            chat_id = req["message"]["chat"]["id"]
+
+            if "message" in req and "text" in req["message"]:
+                message = req["message"]["text"].strip()
+
+                # Passes the city name to the thing.
+                weather_response = weather.get_weather_data(message)
+
+                telegram_response = {
+                    "chat_id": chat_id,
+                    "text": weather_response
+                }
+
+                # Send response
+                response = requests.post(
+                    f"{https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage}",
+                    json=telegram_response,
+                )
+
+                return {"ok": True}
+
+            else:
+                # Handle invalid message format
+                telegram_response = {
+                    "chat_id": chat_id,
+                    "text": "What did you mean by that?"
+                }
+
+                requests.post(
+                    f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
+                    json=telegram_response
+                )
+
+                return {"ok": True}
 
     return app
